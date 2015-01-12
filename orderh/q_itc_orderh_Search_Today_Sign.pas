ec4856050d5b7903f5db8a362baec43938632558
kwestground <kenny@westground.se>
2015-01-12 16:47:35 +0100

    Add q_itc_orderh_Search_Today_Sign

---
+++ orderh/q_itc_orderh_Search_Today_Sign.pas
@@ -0,0 +1,15 @@
+﻿function q_itc_orderh_Search_Today_Sign: boolean;
+begin
+  If(SearchMode(0)) Then
+  Begin
+    oh.OrdDatum := Today;
+    oh.RowCreatedBy := sy2.PersSign;
+    MarkField('oh','orddatum');
+    MarkField('oh','rowcreatedby');
+    MenuCommand(905);
+  End
+  Else
+  Begin
+    Message('Formuläret är ej i sökläge!');
+  End;
+End;                                                  
